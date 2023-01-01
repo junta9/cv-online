fddd45c3cd2f98fa58aa04326735095395233223
chou <chouvang@free.fr>
2023-01-01 18:14:43 +0100

    maj

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-
 // *******Animation navbar qui s'affiche que lorqu'on remonte en scrollbar*******
 let lastScrollTop = 0;
 navbar = document.getElementById("navbar");
 
-window.addEventListener("scroll", function() {
-    const scrollTop = window.pageyoffset ||
-    this.document.documentElement.scrollTop;
+window.addEventListener("scroll", function () {
+  const scrollTop =
+    window.pageyoffset || this.document.documentElement.scrollTop;
 
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top="-50px";
-    } else {
-        navbar.style.top="0";
-    }
-    lastScrollTop = scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-50px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
 });
 
 // ******************TYPE TEXT***************
@@ -31,3 +30,29 @@ function typeWriter() {
   }
 }
 setInterval(typeWriter(), 1000);
+
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
